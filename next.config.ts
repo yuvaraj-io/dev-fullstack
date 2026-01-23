92c93a82fdaa40b22160d57a2fe48721cc469c5a
Yuvaraj S <yuvarajs@Yuvarajs-MacBook-Air.local>
2026-01-23 20:55:24 +0530

    added blog base path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    // basePath: '/blog',
-    // assetPrefix: '/blog',
+    basePath: '/blog',
+    assetPrefix: '/blog',
 };
 
 export default nextConfig;
