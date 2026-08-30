c163af5002d559ea226ac0346fce42b7b2ecdb97
Yuvaraj S <yuvayuvaraj720444@gmail.com>
2026-08-30 18:31:32 +0530

    turbo pack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+import path from "path";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-    // basePath: '/blog',
-    // assetPrefix: '/blog',
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
